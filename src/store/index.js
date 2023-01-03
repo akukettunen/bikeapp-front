03d2e027082a49464fa371e176b7d2ff3a534750
Aku Kettunen <aku@kepit.fi>
2023-01-03 15:17:46 +0200

    initial commit :)

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    trips: [],
+    stations: [],
+    tripsPageSize: 20,
+    stationsPageSize: 20
   },
   mutations: {
+    SET_TRIPS (state, trips) {
+      state.trips = trips
+    },
+    SET_STATIONS (state, stations) {
+      state.stations = stations
+    }
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    trips: state => state.trips,
+    stations: state => state.stations,
+    tripsPageSize: state => state.tripsPageSize,
+    stationsPageSize: state => state.stationsPageSize
   }
 })
